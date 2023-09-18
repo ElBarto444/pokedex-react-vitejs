dbf41f1526134643dd74d5cf7dba843882a6a666
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-09-18 15:30:26 +0200

    added props to PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,10 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
+    console.log(pokemon)
 
   return (
     <figure>
       {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <img src={pokemon.imgSrc} alt={pokemon.imgSrc} />
       ) : (
         <p>???</p>
       )}
