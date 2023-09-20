5c08aa404c8d2c2951c8970d73ea5ea9a9c101b6
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-09-20 16:54:48 +0200

    added useEffect on app start and on pikachu selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,50 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
 
 function App() {
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "charmander",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-  {
-    name: "squirtle",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  },
-  {
-    name: "pikachu",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  },
-  {
-    name: "mew",
-  },
-];
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
   const increment = () => setPokemonIndex(pokemonIndex + 1);
   const decrement = () => setPokemonIndex(pokemonIndex - 1);
+
+  useEffect(() => {
+    alert("hello pokemon trainer :)");
+  }, []);
+
+  useEffect(() => {
+    pokemonList[pokemonIndex].name === "pikachu" ?
+    alert("pika pikachu !!!") : "";
+  }
+
+  )
 
   return (
     <>
