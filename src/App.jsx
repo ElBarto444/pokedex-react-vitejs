8472933a12feeb32d9e97cf4d47bcfb30faa360e
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-09-20 14:48:04 +0200

    added states and prev next buttons to pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+    
+    const increment = () => 
+          setPokemonIndex(pokemonIndex +1)
 
-  const pokemon = pokemonList[0];
+    const decrement = () => 
+         setPokemonIndex(pokemonIndex -1)
 
   return (
-    <>
-    < PokemonCard pokemon = { pokemon } />
-    </>
-  )
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      <button onClick={decrement}>Précedent</button>
+      <button onClick={increment}>Suivant</button>
+    </div>
+  );
 }
 
 export default App;
